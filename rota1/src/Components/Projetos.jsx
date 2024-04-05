e6b313e07879deb8b42b9957535d569e11bb5d4b
Juliana Portella <julinglesaportella@gmai.com>
2024-04-05 17:13:25 -0300

    atualizar

--- rota1/src/Components/Projetos.jsx
+++ rota1/src/Components/Projetos.jsx
@@ -1,35 +1,27 @@
 import React from "react"
 import Reepdex from "./ImagensPort/reepdex.png"
-import Stitch  from "./stitch.jpg"
+import Stitch from "./ImagensPort/stitch.jpg"
+import Logodog from "./ImagensPort/logodog.jpg"
+
+function Projetos() {
+    return (
+
+        <S.Section>
+
+            <S.MinhaDiv>
+                <S.Port src={Reepdex} alt="lojareepdex" />
+                <p>Caminho: <a href="https://www.instagram.com/lojareepdex" target="_blank">https://www.instagram.com/lojareepdex</a></p>
+
+                <S.Port src={Stitch} alt="lojaroupa" />
+                <p>Repositorio: <a href="https://github.com/Julinglesa/desafiovainawev2" target="_blanl">https://github.com/Julinglesa/desafiovainawev2</a></p>
+
+                  
+                  <S.Port src={Logodog} alt ="fantastika"/>
+                  <p>Repositorio : <a href="https://github.com/Julinglesa/desafioFantastika" target ="_blank">https://github.com/Julinglesa/desafioFantastika</a></p>
 
 
-
-
-function Projetos(){
-    return(
-
-<S.ClasSection>
-
-    <S.MinhaDiv>
-        <S.Port src={Reepdex} alt="loja"/>
-        <p>Caminho: <a href="https://www.instagram.com/lojareepdex" target ="_blank">https://www.instagram.com/lojareepdex</a></p>
-
-        <S.Port src={Stitch} alt="lojaroupa"/>
-        <p>Repositorio: <a href="https://github.com/Julinglesa/desafiovainawev2" target ="_blanl">https://github.com/Julinglesa/desafiovainawev2</a></p>
-
-        
-        
-
-
-
-    </S.MinhaDiv>
-</S.ClasSection>
-
-
-
-
-
-
+            </S.MinhaDiv>
+        </S.Section>
 
  )
 }
--- rota1/src/Components/Style.jsx
+++ rota1/src/Components/Style.jsx
@@ -1,3 +1,24 @@
 import Styled from " Styled-components"
 
-const Style 
+
+export const Inicio = styled.inicio`
+
+display: flex;
+width: 80vw;
+height: 60vh;
+justify-content: center;
+flex-direction: row;
+align-items : center;
+`
+export const Img = Styled.img`
+width : 40 vh;
+`
+
+// navegação
+export const Nav = styled.nav`
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+background-color: purple;
+
+`
